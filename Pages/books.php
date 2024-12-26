9fa8673ef40a16a51cba270578dc14dd9cde7981
Juan Miguel Lahorra <lahorrajm@gmail.com>
2024-12-26 18:31:40 +0800

    added pages; bibles.php, books.php, homepage.php, and profile.php

---
+++ Pages/books.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>Books</title>
+</head>
+<body>
+<div class="nav">
+        <h1>Scripture Spreadsheet</h1>
+        <div class="nav-links">
+                <a href="homepage.php" ><i class="fas fa-home"></i> Home</a>
+                <a href="bibles.php"><i class="fas fa-book"></i> Bibles</a>
+                <a href="books.php" class="active"><i class="fas fa-book-open"></i> Books</a>
+                <a href="profile.php" ><i class="fas fa-user"></i> Profile</a>
+        </div>
+    </div>
+</body>
+</html>
